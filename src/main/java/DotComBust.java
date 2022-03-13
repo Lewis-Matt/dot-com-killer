6f8af9a63b71976c3c7b6df24113c5e2ed0d7f24
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-13 17:18:39 -0500

    added GameHelper logic

--- src/main/java/DotComBust.java
+++ src/main/java/DotComBust.java
@@ -1,3 +1,4 @@
+import util.GameHelper;
 import util.SimpleGameHelper;
 
 import java.util.*;
@@ -5,7 +6,7 @@ import java.util.*;
 // Main application - full version
 // Makes instances of DotComs, uses SimpleGameHelper to get input
 public class DotComBust {
-    private SimpleGameHelper helper = new SimpleGameHelper();
+    private GameHelper helper = new GameHelper();
     private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
     private int numOfGuesses = 0;
 
@@ -25,10 +26,8 @@ public class DotComBust {
                 "Your goal is to remove market competition by sinking dot-coms.");
         System.out.printf("Try to sink %s, %s, and %s in the fewest amount of guesses in order to earn your bonus.%n", one.getName(), two.getName(), three.getName());
 
-        for(DotCom dotComToSet : dotComsList) {
+        for (DotCom dotComToSet : dotComsList) {
             ArrayList<String> newLocation = helper.placeDotCom(3);
         }
     }
-
-
 }
--- src/main/java/util/GameHelper.java
+++ src/main/java/util/GameHelper.java
@@ -3,28 +3,83 @@ package util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-// For use with DotComBust and DotCom
+// For use with DotComBust and DotCom (see notes in SimpleGameHelper)
 public class GameHelper {
+    private static final String alphabet = "abcedfg";
+    private int gridLength = 7;
+    private int gridSize = 49;
+    private int[] grid = new int[gridSize];
+    private int comCount = 0;
+
     public String getUserInput(String prompt) {
         String inputLine = null;
         System.out.println(prompt + " ");
         try {
-            // Enables us to minimize the number of I/O operations by reading chunks of characters and storing them in an internal buffer.
-            // InputStreamReader configures BufferedReader to take any kind of input stream as an underlying source.
-            // System.in typically corresponds to the input from the keyboard.
             BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
-            // Reads the content of the file/object
             inputLine = is.readLine();
             if (inputLine.length() == 0) {
                 return null;
             }
-            // General class of exceptions produced by failed or interrupted I/O operations.
         } catch (IOException e) {
             System.out.println("IOException: " + e);
         }
-        return inputLine;
+        return inputLine.toLowerCase();
     }
 
+    public ArrayList<String> placeDotCom(int comSize) {
+        ArrayList<String> alphaCells = new ArrayList<String>();
+
+        String temp = null;
+        int[] coords = new int[comSize];
+        int attempts = 0;
+        boolean success = false;
+        int location = 0;
+
+        comCount++;
+        int incr = 1;
+        if ((comCount % 2) == 1) {
+            incr = gridLength;
+        }
+
+        while (!success & attempts++ < 200) {
+            location = (int) (Math.random() * gridSize);
+//            System.out.println(" try " + location);
+            int x = 0;
+            success = true;
+            while (success && x < comSize) {
+                if (grid[location] == 0) {
+                    coords[x++] = location;
+                    location += incr;
+                    if (location >= gridSize) {
+                        success = false;
+                    }
+                    if (x > 0 && location % gridLength == 0) {
+                        success = false;
+                    }
+                } else {
+//                    System.out.println(" used " + location);
+                    success = false;
+                }
+            }
+        }
+        int x = 0;
+        int row = 0;
+        int column = 0;
+//        System.out.println("\n");
+        while (x < comSize) {
+            grid[coords[x]] = 1;
+            row = (int) (coords[x] / gridLength);
+            column = coords[x] % gridLength;
+            temp = String.valueOf(alphabet.charAt(column));
+
+            alphaCells.add(temp.concat(Integer.toString(row)));
+            x++;
+//            System.out.println(" coord " + x + " = " + alphaCells.get(x - 1));
+        }
+//        System.out.println("\n");
+        return alphaCells;
+    }
 }
-}
+
