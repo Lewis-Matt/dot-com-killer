4a6d4943e47ac784e4e0656f5bcc450e2d7e4404
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-13 13:55:24 -0500

    created GameHelper class that gets user input

---
+++ src/main/java/util/GameHelper.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.io.*;
+
+// This class serves to get the user input
+public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.println(prompt + " ");
+        try {
+            // Enables us to minimize the number of I/O operations by reading chunks of characters and storing them in an internal buffer.
+            // InputStreamReader configures BufferedReader to take any kind of input stream as an underlying source.
+            // System.in typically corresponds to the input from the keyboard.
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            // Reads the content of the file/object
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) {
+                return null;
+            }
+            // General class of exceptions produced by failed or interrupted I/O operations.
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
+
+}
