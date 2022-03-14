5af23c0a37a72e63147abac7f09eaffb9a62a397
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-14 10:01:38 -0500

    finished logic, comments

--- src/main/java/DotComBust.java
+++ src/main/java/DotComBust.java
@@ -1,5 +1,4 @@
 import util.GameHelper;
-import util.SimpleGameHelper;
 
 import java.util.*;
 
@@ -28,6 +27,50 @@ public class DotComBust {
 
         for (DotCom dotComToSet : dotComsList) {
             ArrayList<String> newLocation = helper.placeDotCom(3);
+        } //forEach
+    } // setup
+
+    private void startPlaying() {
+        while (!dotComsList.isEmpty()) {
+            String userGuess = helper.getUserInput("Enter a guess");
+            checkUserGuess(userGuess);
+        } // while
+        finishGame();
+    } // startPlaying
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+
+        String result = "miss";
+
+        for (int x = 0; x < dotComsList.size(); x++) {
+            result = dotComsList.get(x).checkYourself(userGuess);
+
+            if (result.equals("hit")) {
+                break;
+            }
+            if (result.equals("kill")) {
+                dotComsList.remove(x);
+                break;
+            }
+        } // for
+        System.out.println(result);
+    } // checkUserGuess
+
+    private void finishGame() {
+        System.out.println("All Dot Coms are bankrupt! You are now a monopoly!");
+        if (numOfGuesses <= 18) {
+            System.out.println("It only took you " + numOfGuesses + " contracts to bankrupt them!");
+            System.out.println("You were able to insider-trade (ahem, I mean 'speculate') and turn a profit on shorting the dot coms!");
+        } else {
+            System.out.println("Took you long enough... " + numOfGuesses + " contracts had to be signed in order to bankrupt them.");
+            System.out.println("Your short options expired out of the money.");
         }
-    }
-}
+    } // finishGame
+
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    } // main
+} // class
